c8aaa325026218dc5350c6ddc2b171cf02352922
your name <xuanthanh.110699@gmail.com>
2021-09-12 22:16:04 +0700

    up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-$('a[href*="#"]').on('click',function(e){
 
-    e.preventDefault();
 
-    $('html, body').animate({
+function User(firstName, lastName, avatar) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.avatar = avatar;
 
-      scrollTop : $($(this).attr('href')).offset().top,
+  this.getName = function () {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+ 
+var author = new User('Son', 'Dang', 'Avatar');
+var user = new User('Vũ', 'Nguyễn', 'Avatar');
 
-    },
-      500, 
-      'linear'
-    );
+author.title = 'Chia sẻ dạo tại F8';
+user.comment = 'Liệu có khóa asp.net k ad :)';
 
-  });
+console.log(author.getName());
+console.log(user);
