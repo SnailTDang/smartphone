f2a2a9b40a197f87c5d38e820af265c9687074e1
your name <xuanthanh.110699@gmail.com>
2021-09-19 01:30:42 +0700

    up

--- js/app.js
+++ js/app.js
@@ -18,3 +18,25 @@ user.comment = 'Liệu có khóa asp.net k ad :)';
 
 console.log(author.getName());
 console.log(user);
+
+$(document).ready(function(){
+
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
