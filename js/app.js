03596ee885b9fdced5579a77f9afc319282bf7fd
your name <xuanthanh.110699@gmail.com>
2021-09-19 13:09:32 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -21,6 +21,23 @@
 
 $(document).ready(function(){
 
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
 
   // smooth scrolling 
 
@@ -33,7 +50,7 @@ $(document).ready(function(){
       scrollTop : $($(this).attr('href')).offset().top,
 
     },
-      500, 
+      900, 
       'linear'
     );
 
