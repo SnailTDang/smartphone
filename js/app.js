b4bb6eb654da25e4c33a9ccbb006085b4190eac7
your name <xuanthanh.110699@gmail.com>
2021-10-11 11:41:54 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -36,7 +36,52 @@ $(document).ready(function(){
     );
 
   });
+  
 
 });
 
 
+
+
+// var myArray = [
+//   'Javascript',
+//   'PHP',
+// ]
+
+// var i = 0;
+//     count = myArray.length;
+
+// while (i < (count)) {
+//   console.log(myArray[i]);
+//   i++;
+// 
+
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i)
+// }
+
+function run(physics, chemistry, biology, mathematics, computer){
+	var percentage = (chemistry + biology + mathematics + computer)/2;
+	var sz_grade = "";
+	
+	if (percentage >= 9) {
+		sz_grade = 'Grade A';
+	}
+	else if (percentage >= 8) {
+		sz_grade = 'Grade B';
+	}
+	else if (percentage >= 7) {
+		sz_grade = 'Grade C';
+	}
+	else if (percentage >= 6) {
+		sz_grade = 'Grade D';
+	}
+	else if (percentage >= 4) {
+		sz_grade = 'Grade E';
+	} else {
+		sz_grade = 'Grade F';
+	}
+	
+	console.log(sz_grade);
+}
